18448f2c813dc637db1f08a74a55ae99f4041403
bhari eutsey <bharilynn@outlook.com>
2021-12-26 01:50:27 -0600

    modularized html page

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// must have when generating new files
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+//data entrypoints: a name and a username
+const profileDataArgs = process.argv.slice(2);
 
 
-const printProfileData = (profileDataArr) => {
-      
-      profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] =profileDataArgs;
+
+
+
+// actually writing the new file and checking if there are errors or if written successfully
+fs.writeFile('./index.html',generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
+
+
