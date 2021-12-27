0434a71f5c28cbbf8a5a2891a9de2930e64fe7cd
bhari eutsey <bharilynn@outlook.com>
2021-12-27 07:48:01 -0600

    ability to recieve answers

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 // must have when generating new files
-const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-//data entrypoints: a name and a username
-const profileDataArgs = process.argv.slice(2);
+//const fs = require('fs');
 
+//const generatePage = require('./src/page-template.js');
 
-const [name, github] =profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
 
 
 // actually writing the new file and checking if there are errors or if written successfully
-fs.writeFile('./index.html',generatePage(name, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+//fs.writeFile('./index.html',pageHTML, err => {
+    //if (err) throw new Error(err);
+   // console.log('Portfolio complete! Checkout index.html to see the output!');
+//});
 
 
